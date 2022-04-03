c835dd78ddcfd0b7e91ebffe02d80118d802c4c2
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2022-04-03 23:39:16 +0000

    Using getaddrinfo instead of gethostbyname

--- training/server.cpp
+++ training/server.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <strings.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -32,14 +33,26 @@ int main(int argc, char const *argv[]) {
 	struct sockaddr_in	serverAddress, clientAddress;
 	struct hostent		*host;
 	char				buffer[256];
+	struct addrinfo		hints;
+	struct addrinfo		*servInfo;
 
 	if (argc != 3) {
 		std::cerr << "Specify host address and port number" << std::endl;
 		exit(1);
 	}
-	portNum = atoi(argv[2]);
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	
+	if (getaddrinfo(argv[1], argv[2], &hints, &servInfo)) {
+		std::cerr << "Invalid address" << std::endl;
+		exit(1);
+	}
+
+	// portNum = atoi(argv[2]);
 	/* Getting socket fd */
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(servInfo->ai_family, servInfo->ai_socktype, servInfo->ai_protocol);
 	if (sockfd < 0) {
 		std::cerr << "Error opening socket" << std::endl;
 		exit(1);
@@ -52,20 +65,29 @@ int main(int argc, char const *argv[]) {
 	bcopy((char *)host->h_addr_list[0], (char *)&serverAddress.sin_addr.s_addr, host->h_length);
 	serverAddress.sin_port = htons(portNum);
 
+	/* Reuse port  */
+	int	yes = 1;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+		std::cerr << "setsockopt" << std::endl;
+		exit(1);
+	}
+	
 	/* Binding to host address */
-	if (bind(sockfd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+	if (bind(sockfd, servInfo->ai_addr, servInfo->ai_addrlen) < 0) {
 		std::cerr << "Error in binding" << std::endl;
 		exit(1);
 	}
 
 	/* Listening on host address */
-	listen(sockfd, 5);
+	if (listen(sockfd, 5) == -1) {
+		std::cerr << "Error in listening" << std::endl;
+		exit(1);
+	}
 	unsigned int clientLen = sizeof(clientAddress);
 
 	while (1) {
 		/* Accepting connections */
-		clientfd = accept(sockfd, (struct sockaddr *)&clientAddress, &clientLen);
-		if (clientfd < 0) {
+		if ((clientfd = accept(sockfd, (struct sockaddr *)&clientAddress, &clientLen)) < 0) {
 			std::cerr << "Error accepting request" << std::endl;
 			exit(1);
 		}
