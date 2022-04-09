8762fcc185ce7c01c877f8eeaec2ec2eb598484d
Barimehdi77 <mbari@student.1337.ma>
2022-04-09 02:05:55 +0000

    backup

--- learning/mini_IRC/Client.cpp
+++ learning/mini_IRC/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 01:09:14 by mbari             #+#    #+#             */
-/*   Updated: 2022/04/09 01:50:07 by mbari            ###   ########.fr       */
+/*   Updated: 2022/04/09 02:05:41 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@
 Client::Client(): _username(std::string()), _clientfd(0), _isLoggedIn(0) {};
 
 
-Client & Client::operator=( const Client& rhs)
-{
-	std::cout << "zb";
-	return (*this);
-}
+// Client & Client::operator=( const Client& rhs)
+// {
+// 		return (*this);
+// }
 
 Client::~Client() {};
 
