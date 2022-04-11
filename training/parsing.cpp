6a890aa34511a6497a95c3e4b6b91983d513aafc
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2022-04-11 13:28:08 +0000

    Finished parsing request

--- training/parsing.cpp
+++ training/parsing.cpp
@@ -8,48 +8,45 @@ private:
 
 public:
 	std::vector<std::string>	args;
+	std::string					command;
 	Request() {}
 };
 
-void	parseRequest(std::string str) {
-	// std::string s = "scott>=tiger>=mushroom";
-	// std::string delimiter = ">=";
-
-	// size_t pos = 0;
-	// std::string token;
-	// while ((pos = s.find(delimiter)) != std::string::npos) {
-	// 	token = s.substr(0, pos);
-	// 	std::cout << token << std::endl;
-	// 	s.erase(0, pos + delimiter.length());
-	// }
-	// std::cout << s << std::endl;
-	
+Request	parseRequest(std::string str) {
 	Request	request;
 	size_t	i = 0;
 	size_t	j = 0;
-	size_t	k = 0;
 
+	while (str[i] && str[i] == ' ')
+		i++;
+	j = i;
 	while (str[i]) {
 		if (str[i] == ' ') {
 			request.args.push_back(str.substr(j, i - j));
-			if (str[i] == ' ')
+			while (str[i] == ' ')
 				i++;
 			j = i;
+		}
+		if (str[i] == ':') {
+			request.args.push_back(str.substr(i + 1, str.length() - i));
+			request.command = request.args[0];
+			request.args.erase(request.args.begin());
+			return request;
 		}
 		i++;
 	}
 	request.args.push_back(str.substr(j, i - j));
-	std::cout << str;
-
-
-	// while (str[i]) {
-	// 	if (str[i] == ' ')
-	// 		std::cout << "Gazoinks";
-	// 	i++;
-	// }
+	request.command = request.args[0];
+	request.args.erase(request.args.begin());
+	return request;
 }
 
 int		main() {
-	parseRequest("NICK amy");
+	Request	request;
+	request = parseRequest("      NICK amy      fhdsjkfew hjkds fewjkfw");
+	std::cout << "COMMAND: " << request.command << std::endl;
+	std::cout << "ARGS:\n";
+		for (size_t i = 0; i < request.args.size(); i++)
+		std::cout << i + 1 <<") " << request.args[i] << std::endl;
 	return 0;
 }
