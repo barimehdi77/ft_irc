a93a1f0d1d7f1f6e25ba0f2d3fdda774779a5ee7
Barimehdi77 <mbari@student.1337.ma>
2022-04-18 23:48:24 +0000

    add file for command functions

--- headers/Server.hpp
+++ headers/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 23:32:10 by mbari             #+#    #+#             */
-/*   Updated: 2022/04/18 16:45:50 by mbari            ###   ########.fr       */
+/*   Updated: 2022/04/18 23:45:50 by mbari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@
 
 class Request
 {
-public:
-	std::vector<std::string>	args;
-	std::string					command;
-	bool						invalidMessage;
+	public:
+		std::vector<std::string>	args;
+		std::string					command;
+		bool						invalidMessage;
 
 	Request() : args(), command(), invalidMessage(false) {}
 };
@@ -30,52 +30,53 @@ public:
 
 class Server
 {
-private:
-	std::string		_name;
-	int				_socketfd;
-	Client			*_clients;
-	struct pollfd	*_pfds;
-	int				_online_c;
-	int				_max_online_c;
-	std::string		_prefix;
+	private:
+		std::string						_name;
+		std::vector<std::string>		_unavailableUserName;
+		int								_socketfd;
+		Client							*_clients;
+		struct pollfd					*_pfds;
+		int								_online_c;
+		int								_max_online_c;
+		std::string						_prefix;
 
-private:
-	Server();
-	std::string						_welcomemsg(void);
-	void							_getSocket(std::string Port);
-	void							_addToPoll(int newfd);
-	void							_removeFromPoll(int i);
-	void							_newClient(void);
-	void							_ClientRequest(int i);
-	int								_sendall(int destfd, std::string message);
-	void							_broadcastmsg(int sender_fd, std::string buf, int nbytes);
-	std::string						_setUserName(Request request, int i);
-	std::string						_setNickName(Request request, int i);
-	std::string						_setFullName(Request request, int i);
-	std::string						_setPassWord(Request request, int i);
-	std::string						_quit(Request request, int i);
-	std::string						_sendMessage(std::string message, int i);
-	std::string						_printError(int num, std::string reply, std::string message);
-	// std::string						_setUsername( std::string username, int i );
-	std::string						_parsing(std::string message, int i);
-	Request							_splitRequest(std::string req);
-	std::string						_printUserInfo(int i);
-	std::string						_printHelpInfo(int i);
-	std::string						_printReply(int num, std::string reply, std::string message);
+	private:
+		Server();
+		std::string						_welcomemsg(void);
+		void							_getSocket(std::string Port);
+		void							_addToPoll(int newfd);
+		void							_removeFromPoll(int i);
+		void							_newClient(void);
+		void							_ClientRequest(int i);
+		int								_sendall(int destfd, std::string message);
+		void							_broadcastmsg(int sender_fd, std::string buf, int nbytes);
+		std::string						_setUserName(Request request, int i);
+		std::string						_setNickName(Request request, int i);
+		std::string						_setFullName(Request request, int i);
+		std::string						_setPassWord(Request request, int i);
+		std::string						_quit(Request request, int i);
+		std::string						_sendMessage(std::string message, int i);
+		std::string						_printError(int num, std::string reply, std::string message);
+		// std::string						_setUsername( std::string username, int i );
+		std::string						_parsing(std::string message, int i);
+		Request							_splitRequest(std::string req);
+		std::string						_printUserInfo(int i);
+		std::string						_printHelpInfo(int i);
+		std::string						_printReply(int num, std::string reply, std::string message);
 
-// private:
-// 	class ArgsError: public std::exception
-// 	{
-// 		virtual const char* what() const throw();
-// 	};
-public:
-	Server(std::string Name, int max_online, std::string Port);
-	Server(const Server & x);
-	~Server();
-	Server & operator=(const Server & rhs);
+	// private:
+	// 	class ArgsError: public std::exception
+	// 	{
+	// 		virtual const char* what() const throw();
+	// 	};
+	public:
+		Server(std::string Name, int max_online, std::string Port);
+		Server(const Server & x);
+		~Server();
+		Server & operator=(const Server & rhs);
 
-public:
-	void	startServer(void);
+	public:
+		void	startServer(void);
 };
 
 
---
+++ srcs/commands.cpp
@@ -0,0 +1,152 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   commands.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbari <mbari@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/18 23:46:52 by mbari             #+#    #+#             */
+/*   Updated: 2022/04/18 23:47:57 by mbari            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/Server.hpp"
+
+
+std::string	Server::_parsing(std::string message, int i)
+{
+	Request	request(_splitRequest(message));
+
+	// if (request.command == "HELP")
+	// 	return ("To login you need to request 'USERNAME (your_username)'\n");
+	// else if (request.command == "USERNAME")
+	// 	return (_setUsername(request.args, i));
+	// else if (request.command == "MESSAGE")
+	// 	return (_sendMessage(request.args, i));
+	// else
+	// 	return ("Command not found\nUsage: USERNAME (your_username)\n");
+	// std::cout << "command: " << request.command << std::endl;
+	if (request.invalidMessage)
+		return ("Invalid message!\n");
+	if (request.command == "PASS")
+		return (_setPassWord(request, i));
+	else if (request.command == "NICK")
+		return (_setNickName(request, i));
+	else if (request.command == "USER")
+		return (_setUserName(request, i));
+	else if (request.command == "PRIVMSG")
+		return ("PRIVMSG command\n");
+	else if (request.command == "HELP")
+		return (_printHelpInfo(i));
+	else if (request.command == "JOIN")
+		return ("JOIN command\n");
+	else if (request.command == "KICK")
+		return ("KICK command\n");
+	else if (request.command == "QUIT")
+		return (_quit(request, i));
+	else if (request.command == "INFO")
+		return (_printUserInfo(i));
+	else
+		return ("Invalid command\n");
+};
+
+std::string	Server::_setPassWord(Request request, int i)
+{
+	if (request.args.size() < 1)
+		return (_printError(461, "ERR_NEEDMOREPARAMS", "PASS :Not enough parameters"));
+	if (this->_clients[i].getRegistered())
+		return (_printError(462, "ERR_ALREADYREGISTRED", ":Unauthorized command (already registered)"));
+	this->_clients[i].setPassWord(request.args[0]);
+	return ("PassWord is set\n");
+};
+
+std::string	Server::_setNickName(Request request, int i)
+{
+	std::cout << "args: " << request.args[0] << std::endl;
+	if (request.args.size() < 1)
+		return (_printError(431, "ERR_NONICKNAMEGIVEN", ":No nickname given"));
+	int	j = 0;
+	while (request.args[0][j])
+	{
+		if (!isalnum(request.args[0][j]) && request.args[0][j] != '-')
+			return (_printError(432, "ERR_ERRONEUSNICKNAME", request.args[0] + " :Erroneous nickname"));
+		j++;
+	}
+	// std::cout << RED << "while is done " << RESET << std::endl;
+	this->_clients[i].setNickName(request.args[0]);
+	if (this->_clients[i].getUserName() != "") {
+		this->_clients[i].setID(this->_clients[i].getNickName() + "!" + this->_clients[i].getUserName() + "@" + this->_clients[i].getHost());
+		this->_clients[i].setRegistered(true);
+		return (_printReply(1, "RPL_WELCOME", "Welcome to the Internet Relay Network " + this->_clients[i].getID()));
+	}
+	return ("");
+};
+
+std::string	Server::_setUserName(Request request, int i)
+{
+	if (this->_clients[i].getRegistered())
+		return (_printError(462, "ERR_ALREADYREGISTRED", ":Unauthorized command (already registered)"));
+	if (request.args.size() < 4)
+		return (_printError(461, "ERR_NEEDMOREPARAMS", "USER :Not enough parameters"));
+	this->_clients[i].setUserName(request.args[0]);
+	this->_clients[i].setFullName(request.args[3]);
+	if (this->_clients[i].getNickName() != "") {
+		this->_clients[i].setID(this->_clients[i].getNickName() + "!" + this->_clients[i].getUserName() + "@" + this->_clients[i].getHost());
+		this->_clients[i].setRegistered(true);
+		return (_printReply(1, "RPL_WELCOME", "Welcome to the Internet Relay Network " + this->_clients[i].getID()));
+	}
+	return ("");
+};
+
+std::string	Server::_quit(Request request, int i)
+{
+	std::string ret = ":" + this->_clients[i].getID() + " QUIT ";
+	if (request.args.size())
+		ret.append(":" + request.args[0] + "\n");
+		// return (ret.append(":" + request.args[0] + "\n"));
+	else
+		ret.append("\n");
+		// return (ret.append("\n"));
+	_broadcastmsg(this->_clients[i].getClientfd(), ret, ret.length());
+	close(this->_clients[i].getClientfd());
+	_removeFromPoll(i);
+	return ("QUIT");
+};
+
+std::string	Server::_printHelpInfo(int i)
+{
+	std::string	helpInfo;
+
+	helpInfo.append(GREEN);
+	helpInfo.append("STEP 1: PASS (Optional)\n");
+	helpInfo.append(RESET);
+	helpInfo.append("\tUse PASS command to set a password. e.g: PASS MyAwesomePassword123\n\n");
+	helpInfo.append(GREEN);
+	helpInfo.append("STEP 2: NICK\n");
+	helpInfo.append(RESET);
+	helpInfo.append("\tUse NICK command to set a nickname. e.g: NICK deezNuts69\n\n");
+	helpInfo.append(GREEN);
+	helpInfo.append("STEP 3: USER\n");
+	helpInfo.append(RESET);
+	helpInfo.append("\tUse USER command to register your username and fullname.e.g: USER deez * * : Deez Nuts\n\n");
+	return (helpInfo);
+};
+
+
+std::string	Server::_printUserInfo(int i)
+{
+	std::string info;
+	info.append("NickName: " + this->_clients[i].getNickName() + "\n");
+	info.append("UserName: " + this->_clients[i].getUserName() + "\n");
+	info.append("FullName: " + this->_clients[i].getFullName() + "\n");
+	info.append("Host: " + this->_clients[i].getHost() + "\n");
+	info.append("ID: " + this->_clients[i].getID() + "\n");
+	info.append("PassWord: " + this->_clients[i].getPassWord() + "\n");
+	// info << "Nick: " << client._Nick << std::endl;
+	// info << "UserName: " << client._UserName << std::endl;
+	// info << "FullName: " << client._FullName << std::endl;
+	// info << "Host: " << client._Host << std::endl;
+	// info << "PassWord: " << client._PassWord << std::endl;
+	// info << "ID: " << client._ID << std::endl;
+	return (info);
+};
